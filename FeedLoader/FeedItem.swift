5baeec90fb43a0c527c1c67f2e11edfd678f0ef2
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-02-14 16:41:09 +0100

    Add FeedItem

---
+++ FeedLoader/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FeedLoader
+//
+//  Created by Fernando Cardenas on 14.02.21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
