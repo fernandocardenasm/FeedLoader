c18d9047a1db1e2fe76bfce97b813cd064683583
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-02-15 16:55:59 +0100

    Add FeedLoader

---
+++ FeedLoaderTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  FeedLoaderTests
+//
+//  Created by Fernando Cardenas on 14.02.21.
+//
+
+import XCTest
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    class RemoteFeedLoader {}
+    
+    func test_init() {
+        let sut = RemoteFeedLoader()
+    }
+}
