0e5b2d41af5469beef8e023ecc0fc2ba0faa4910
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-02-28 11:44:39 +0100

    Add EssentialFeedAPIEndToEndTests Target

--- FeedLoader/HTTPClient.swift
+++ FeedLoader/HTTPClient.swift
@@ -12,3 +12,26 @@ public typealias HTTPClientResult = (Result<(Data, HTTPURLResponse), Error>)
 public protocol HTTPClient {
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
+
+final public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success((data, response)))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
