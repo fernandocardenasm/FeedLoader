76050cacfd41555f3167abc9a4eaa38f2a3d6b2e
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-03-04 15:46:04 +0100

    save command does not trigger insertion when delete completes with error

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -5,25 +5,89 @@
 //  Created by Fernando Cardenas on 04.03.21.
 //
 
+import EssentialFeed
 import XCTest
 
 class CacheFeedUseCaseTests: XCTestCase {
     
     class FeedStore {
-        var deleteCachedFeedCallCount = 0
+        var deleteCachedFeedCompletions = [(Error?) -> Void]()
+        var insertCallCount = 0
+        
+        func deleteCachedFeed(completion: @escaping (Error?) -> Void) {
+            deleteCachedFeedCompletions.append(completion)
+        }
+        
+        func insert(_ items: [FeedItem]) {
+            insertCallCount += 1
+        }
+        
+        func complete(withError error: Error, at index: Int = 0) {
+            deleteCachedFeedCompletions[index](error)
+        }
     }
     
     class LocalFeedLoader {
+        private let store: FeedStore
+        
+        
         init(store: FeedStore) {
-            
+            self.store = store
+        }
+        
+        func save(_ items: [FeedItem]) {
+            store.deleteCachedFeed { error in
+                if let _ = error {
+                } else {
+                    self.store.insert(items)
+                }
+            }
         }
     }
     
     
-    func test_init_doesNotDeleteCacheUponCreation() {
-        let store = FeedStore()
-        let _ = LocalFeedLoader(store: store)
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let (_, store) = makeSUT()
         
-        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+        XCTAssertEqual(store.deleteCachedFeedCompletions.count, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let (sut, store) = makeSUT()
+        
+        sut.save([uniqueItem(), uniqueItem()])
+        
+        XCTAssertEqual(store.deleteCachedFeedCompletions.count, 1)
+    }
+    
+    func test_save_doesNotRequestInsertionOnDeletionError() {
+        let (sut, store) = makeSUT()
+        
+        sut.save([uniqueItem()])
+        
+        let deletionError = anyError()
+        store.complete(withError: deletionError)
+        
+        XCTAssertEqual(store.insertCallCount, 0)
+    }
+    
+    private func makeSUT() -> (LocalFeedLoader, FeedStore) {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        return (sut, store)
+    }
+    
+    private func uniqueItem() -> FeedItem {
+        FeedItem(
+            id: UUID(),
+            description: "some description",
+            location: "some location",
+            imageURL: URL(string: "https://some-url.com")!
+        )
+    }
+    
+    private func anyError() -> Error {
+        NSError(domain: "any", code: 200)
     }
 }
