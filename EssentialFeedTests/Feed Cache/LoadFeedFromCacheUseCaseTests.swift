4082316c65054858cf18bdb963bab843b31ba10d
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-03-07 20:23:12 +0100

    add emtpy tests that should pass for loadFeedFromUseCase

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -10,4 +10,42 @@ import EssentialFeed
 
 class LoadFeedFromCacheUseCaseTests: XCTestCase {
     
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    func test_load_requestsCacheRetrieval() {
+    }
+    
+    func test_load_failsOnRetrievalError() {
+        
+    }
+    
+    func test_load_deliversNoImagesOnEmptyCache() {
+        
+    }
+    
+    func test_load_deliversCachedImagesOnLessThanSevenDaysOldCache() {
+        
+    }
+    
+    func test_load_deliversNoImagesOnSevenDaysOldCache() {
+        
+    }
+    
+    func test_load_deliversNoImagesOnMoreThanSevenDaysOldCache() {
+        
+    }
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (LocalFeedLoader, FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(store, file: file, line: line)
+        
+        return (sut, store)
+    }
 }
