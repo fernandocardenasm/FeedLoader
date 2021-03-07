3ff7a8030d688606fb294576bddc25ae3d7ec61f
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-03-07 20:19:40 +0100

    create file tests for LoadFeedFromCacheUseCase

---
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,13 @@
+//
+//  LoadFeedFromCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Fernando Cardenas on 07.03.21.
+//
+
+import XCTest
+import EssentialFeed
+
+class LoadFeedFromCacheUseCaseTests: XCTestCase {
+    
+}
