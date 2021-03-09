b265992c0cdfa4aed47973a89f59d0ef7b7012b4
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-03-09 15:17:06 +0100

    Add file for ValidateFeedCacheUseCaseTests

---
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift.swift
@@ -0,0 +1,13 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift.swift
+//  EssentialFeedTests
+//
+//  Created by Fernando Cardenas on 09.03.21.
+//
+
+import EssentialFeed
+import XCTest
+
+class ValidateFeedCacheUseCaseTests: XCTestCase {
+    
+}
