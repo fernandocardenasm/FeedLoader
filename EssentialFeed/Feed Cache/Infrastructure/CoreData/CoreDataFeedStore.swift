cf94f8df4ea386d7c42a7d834456b598d93dc3be
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-03-21 17:59:46 +0100

    Fix failing test for EssentialFeedCacheIntegrationTests

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -17,34 +17,31 @@ public final class CoreDataFeedStore: FeedStore {
         context = container.newBackgroundContext()
     }
     
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+    public func retrieve(completion: @escaping RetrievalCompletion) {
         perform { context in
             completion(Result {
-                try ManagedCache.find(in: context).map(context.delete).map(context.save)
+                try ManagedCache.find(in: context).map {
+                    return CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
+                }
             })
         }
     }
     
-    public func insert(_ images: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         perform { context in
-            
             completion(Result {
                 let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
-                managedCache.feed = ManagedFeedImage.images(from: images, in: context)
-                
+                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
                 try context.save()
             })
         }
     }
     
-    public func retrieve(completion: @escaping RetrievalCompletion) {
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         perform { context in
-            
             completion(Result {
-                try ManagedCache.find(in: context).map {
-                    return CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
-                }
+                try ManagedCache.find(in: context).map(context.delete).map(context.save)
             })
         }
     }
